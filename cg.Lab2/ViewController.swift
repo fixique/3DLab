8719b8b9d971344db20586cca8f04da836d19f60
Vlad Krupenko <dev.fixique@gmail.com>
2016-12-12 15:24:00 +0300

    Initial Commit

---
+++ cg.Lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cg.Lab2
+//
+//  Created by Vlad Krupenko on 12.12.16.
+//  Copyright © 2016 fixique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
